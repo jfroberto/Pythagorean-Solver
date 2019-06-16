985e564109cf2ecbf2861f06290ea5392164abff
jfroberto <46858715+jfroberto@users.noreply.github.com>
2019-06-16 16:23:42 +0800

    first python script
    script

---
+++ v1.py
@@ -0,0 +1,31 @@
+import math
+from math import sqrt
+
+print ('Pyhtagorean Theoream')
+print ('     *  ')
+print ('   * *  ')
+print ('C *  * B')
+print (' *   *  ')
+print ('******  ')
+print ('   A    ')
+
+print ('Which side is missing: ')
+missing = input('Type which side is Missing: ')
+
+if missing == 'A':
+    num_B = input('Enter side B: ')
+    num_C = input('Enter Side C: ')
+    num_A= math.sqrt ((-float(num_B)**2) + (float(num_C)**2))
+    print (num_A)
+
+elif missing == 'B':
+    num_A = input('Enter side A: ')
+    num_C = input('Enter Side C: ')
+    num_B = sqrt((-float(num_A) ** 2) + (float(num_C) ** 2))
+    print(num_C)
+
+elif missing == 'C':
+    num_A = input('Enter side A: ')
+    num_B = input('Enter Side B: ')
+    num_C = sqrt((float(num_A) ** 2) + (float(num_B) ** 2))
+    print(num_C)
